905e799a402beee6ca19c7efb0de855fa92f5658
Marco Negroni <lordmarkdev@gmail.com>
2024-04-01 20:03:16 +0200

    -Deployment data

--- scripts/deployWithProxy-defender-CIPToken.ts
+++ scripts/deployWithProxy-defender-CIPToken.ts
@@ -14,17 +14,13 @@ async function main() {
   const deployment = await defender.deployProxy(
     cipToken,
     [
-      "0xdD8ae8624275D68e25869457037277DCd4E0082c", //Init owner
+      "0xCf56D2a736940363167Dbf48925E3Db94d9A4cB9", //Init owner
       [
         //Init recipients
         [
-          "0xdD8ae8624275D68e25869457037277DCd4E0082c",
-          ethers.parseUnits("200000000", 18),
-        ],
-        [
-          "0xb98B6373f7f59bdc0bB2DCd11a9fC1e0a43AfBfa",
-          ethers.parseUnits("100000000", 18),
-        ],
+          "0xCf56D2a736940363167Dbf48925E3Db94d9A4cB9",
+          ethers.parseUnits("300000000", 18),
+        ]
       ],
     ],
     { initializer: "initialize" }
